2fe70258d38dc458f96f802f90720fbe9f7c9036
Shamim Ahmed <shamimahmed30082@gmail.com>
2024-10-04 23:53:26 +0600

    added and imported necessary packages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import cors from 'cors'
+import {v4 as uuidv4} from 'uuid'
 
 const app = express()
 
-app.get('/', (req , res )=>
+app.get('/', (req , res:any ) =>{
     console.log("hello")
+    res.json({
+        hello: "hello world"
+    })
+}
 )
 
 app.listen(8000, ()=> console.log("listening on http://localhost:8000"))
